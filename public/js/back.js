1da3309bea06b82461595694d8b4af788aa97a23
LeppoXena <francesco.lepore@xenagames.com>
2022-09-12 13:06:55 +0200

    Test Francesco

--- public/js/back.js
+++ public/js/back.js
@@ -27920,11 +27920,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-module.exports = __webpack_require__(/*! C:\Users\omarb\Boolean\2-Git-Hub\8-ProgettoFinale\laravel-boolbnb\resources\js\back.js */"./resources/js/back.js");
-=======
-module.exports = __webpack_require__(/*! C:\Users\Utente\Desktop\Boolean\Esercizi\php\laravel-boolbnb\resources\js\back.js */"./resources/js/back.js");
->>>>>>> 60ef4ea34a2d2145df36e352e4e76b841cb12d35
+module.exports = __webpack_require__(/*! D:\Leppo\Varie Leppo\Corso fullstack con Boleean\Esercizi corso\#Progetto finale\File progetto\laravel-boolbnb\resources\js\back.js */"./resources/js/back.js");
 
 
 /***/ })
