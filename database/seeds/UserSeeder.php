ef678a8d06daf5e07d9c7de72eed0d8026d5b529
Claudio <claudiovarrosi@gmail.com>
2022-09-12 13:49:35 +0200

    UserSeeder Created

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $users = [
+            [
+                'name'      => 'Claudio Varrosi',
+                'email'     => 'claudiovarrosi@gmail.com',
+                'password'  => Hash::make('ClaudioVarrosi'),
+            ],
+            [
+                'name'      => 'Francesco Lepore',
+                'email'     => 'francescolepore@gmail.com',
+                'password'  => Hash::make('FrancescoLepore'),
+            ],
+            [
+                'name'      => 'Marco Amico',
+                'email'     => 'marcoamico@gmail.com',
+                'password'  => Hash::make('MarcoAmico'),
+            ],
+            [
+                'name'      => 'Omar Buzzoni',
+                'email'     => 'omarbuzzoni@gmail.com',
+                'password'  => Hash::make('OmarBuzzoni'),
+            ],
+            [
+                'name'      => 'Mirko Simonetti',
+                'email'     => 'mirkosimonetti@gmail.com',
+                'password'  => Hash::make('Mirko Simonetti'),
+            ],
+
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
