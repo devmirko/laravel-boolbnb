5a3ce8005a54663a4563ac5ececddd29ad5367ce
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-09-12 17:42:04 +0200

    inserimento foto nella tabella photos

--- database/seeds/PhotoSeeder.php
+++ database/seeds/PhotoSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\House;
+use App\Models\Photo;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class PhotoSeeder extends Seeder
@@ -9,8 +12,52 @@ class PhotoSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+
+        $arrHouseId = House::all()->pluck('id');
+
+        $arrHouse = [
+            [
+
+                'url' => 'https://i.imgur.com/xbAssQa.jpg'
+
+            ],
+
+            [
+
+                'url' => 'https://i.imgur.com/2pFZMfR.jpg'
+
+            ],
+
+            [
+
+                'url' => 'https://i.imgur.com/zsugY8Z.jpg'
+
+            ],
+
+            [
+
+                'url' => 'https://i.imgur.com/tcZGxPe.jpg'
+
+            ],
+
+            [
+
+                'url' => 'https://i.imgur.com/8N4XGlg.jpg'
+
+            ],
+
+
+           ];
+
+           foreach ( $arrHouse as  $house ) {
+            Photo::create($house +[
+                'house_id' => $faker->randomElement($arrHouseId)
+            ]);
+           }
+
+
 
     }
 }
