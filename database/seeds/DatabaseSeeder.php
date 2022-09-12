ff02445b273f9282a119dbda90cd6669e3cd8355
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-09-12 16:51:28 +0200

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UserSeeder::class);
          $this->call(HouseSeeder::class);
+         $this->call(MessageSeeder::class);
          $this->call(SponsorshipSeeder::class);
          $this->call(ServiceSeeder::class);
     }
