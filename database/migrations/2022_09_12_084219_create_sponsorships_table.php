b8fd56f4fa1592bf784548f9cbe3334499ab8299
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-09-12 11:47:40 +0200

    table sponsorship

--- database/migrations/2022_09_12_084219_create_sponsorships_table.php
+++ database/migrations/2022_09_12_084219_create_sponsorships_table.php
@@ -14,7 +14,11 @@ class CreateSponsorshipsTable extends Migration
     public function up()
     {
         Schema::table('sponsorships', function (Blueprint $table) {
-            //
+            $table->id();
+            $table->string('plan');
+            $table->mediumInteger('price');
+            $table->tinyInteger('duration');
+            $table->timestamps();
         });
     }
 
