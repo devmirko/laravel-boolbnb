cc2d80b7de13553365b5a646461b8a8029dc1eef
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-09-21 21:39:03 +0200

    aggiustamento

--- database/seeds/HouseSponsorshipSeeder.php
+++ database/seeds/HouseSponsorshipSeeder.php
@@ -18,6 +18,24 @@ class HouseSponsorshipSeeder extends Seeder
         // creo una variabile in cui conto gli elementi(id) di sponsorships
         $nSponsorships = count($sponsorships);
 
+        // prendo i valori dell elemento 'duration'
+        $durationSponsor = Sponsorship::all()->pluck('duration');
+
+        // prendo il giorno odierno
+        $giornoOdierno = date("d-m-y");
+
+
+        if ($durationSponsor == '24 Hours'){
+            // se la duration è 24 ore, aggiungo 2 a una variabile
+            $date1 = date("Y-m-d", strtotime($giornoOdierno.'+ 1 days'));
+        } else if ($durationSponsor == '72 Hours'){
+            // se la duration è 72 ore, aggiungo 3 a una variabile
+            $date1 = date("Y-m-d", strtotime($giornoOdierno.'+ 2 days'));
+        } else {
+            // se la duration è 144 ore
+            $date1 = date("Y-m-d", strtotime($giornoOdierno.'+ 4 days'));
+        }
+
         // ciclo sulla tabella house
         foreach ($houses as $house) {
             // creo una variabile in cui assegno random gli id della tabella sponsorships
@@ -25,6 +43,8 @@ class HouseSponsorshipSeeder extends Seeder
             foreach ($houseSponsorships as $sponsorId) {
                 // colleghiamo una serie di id di sponsorships ad un id house
                 $house->sponsorships()->attach($sponsorId);
+                // collego a ogni id di house e sponsorship il valore di epire
+                $house->$date1;
             }
         }
 
