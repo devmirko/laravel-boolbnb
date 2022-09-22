7ba54071ce8e3a3bf26bceb87dbfc8cbfaeb5cae
LeppoXena <francesco.lepore@xenagames.com>
2022-09-22 18:01:47 +0200

    Validation create

--- resources/views/admin/houses/create.blade.php
+++ resources/views/admin/houses/create.blade.php
@@ -2,104 +2,96 @@
 
 @section('mainContent')
     <h1>Inserisci una nuova struttura</h1>
-    <form action="{{ route('admin.houses.store') }}" method="post" novalidate enctype="multipart/form-data">
+    <form action="{{ route('admin.houses.store') }}" method="post" novalidate enctype="multipart/form-data" id="form-new-house">
+    
         @csrf
 
         {{-- nome casa --}}
         <div class="mb-3">
             <label class="form-label" for="name_house">Nome Casa *</label>
             <input class="form-control @error('name_house') is-invalid @enderror" type="text" name="name_house"
-                id="name_house" value="{{ old('name_house')}}" required placeholder="inserisci il nome" minlength="1" maxlength="15">
+                id="name_house" value="{{ old('name_house')}}" required
+                placeholder="inserisci il nome"
+                minlength="1"
+                maxlength="15">
             @error('name_house')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
+
         {{-- numero stanze --}}
         <div class="mb-3">
             <label class="form-label" for="rooms">Numero Stanze *</label>
-            <input class="form-control @error('rooms') is-invalid @enderror" type="number" name="rooms" id="rooms"
-                value="{{ old('rooms')}}" required min="1" max="10">
+            <input class="form-control @error('rooms') is-invalid
+            @enderror" type="number" name="rooms" id="rooms"
+                value="{{ old('rooms')}}" required
+                min="1"
+                max="10">
             @error('rooms')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
+
         {{-- numero letti --}}
         <div class="mb-3">
             <label class="form-label" for="beds">Numero Letti *</label>
-            <input class="form-control @error('beds') is-invalid @enderror" type="number" name="beds" id="beds"
-                value="{{ old('beds')}}" required min="1" max="10">
+            <input class="form-control @error('beds') is-invalid
+            @enderror" type="number" name="beds" id="beds"
+                value="{{ old('beds')}}" required
+                min="1"
+                max="10">
             @error('beds')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
+
         {{-- numero bagni --}}
         <div class="mb-3">
             <label class="form-label" for="bathrooms">Numero Bagni *</label>
             <input class="form-control @error('bathrooms') is-invalid @enderror" type="number" name="bathrooms"
-                id="bathrooms" value="{{ old('bathrooms')}}" required min="1" max="10">
+                id="bathrooms" value="{{ old('bathrooms')}}" required
+                min="1"
+                max="10">
             @error('bathrooms')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
+
         {{-- metri quadrati --}}
         <div class="mb-3">
             <label class="form-label" for="mq">Metri Quadrati *</label>
-            <input class="form-control @error('mq') is-invalid @enderror" type="number" name="mq" id="mq"
-                value="{{ old('mq')}}" required min="1" max="500">
+            <input class="form-control
+            @error('mq') is-invalid @enderror" type="number" name="mq"
+                id="mq" value="{{ old('mq')}}" required
+                min="1"
+                max="500">
             @error('mq')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
-        {{-- <div class="mb-3">
-            <label class="form-label" for="latitude">Latitude *</label>
-            <input class="form-control @error('latitude') is-invalid @enderror" type="text" name="latitude" id="latitude" value="">
-            @error('title')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="longitude">Longitudine *</label>
-            <input class="form-control @error('longitude') is-invalid @enderror" type="text" name="longitude" id="longitude" value="">
-            @error('title')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div> --}}
-        {{-- indirizzo --}}
-        {{-- <div class="mb-3">
-            <label class="form-label" for="address">Indirizzo *</label>
-            <input class="form-control @error('address') is-invalid @enderror" type="text" name="address" id="address"
-                value="" required minlength="1" maxlength="20">
-            @error('address')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div> --}}
 
         {{-- Indirizzo con autocompletamento --}}
-        <div id="back">
+        <div class="mb3" id="back">
             <find-address></find-address>
         </div>
+        <br>
 
         {{-- tipo di struttura --}}
         <div class="mb-3">
-            <label class="form-label" for="type">Tipo di Struttura: *</label>
-            <input class="form-control @error('type') is-invalid @enderror" type="text" name="type" id="type"
-                value="{{ old('type')}}" required pattern="[Aa]ppartamento|[Aa]ttico|[Vv]illa|[Mm]asseria|[Cc]asale"
+            <label class="form-label" for="type">Tipo di Struttura *</label>
+            <input class="form-control @error('type') is-invalid @enderror" type="text" name="type" 
+                id="type" value="{{ old('type')}}" required
+                pattern="[Aa]ppartamento|[Aa]ttico|[Vv]illa|[Mm]asseria|[Cc]asale"
                 placeholder="Appartamento, Attico, Villa, Masseria, Casale.">
             @error('type')
                 <div class="invalid-feedback">
@@ -108,67 +100,66 @@
             @enderror
         </div>
 
-
         {{-- immagini --}}
         <div class="mb-3">
-            <label class="form-label" for="cover_photo">Image</label>
+            <label class="form-label" for="cover_photo">Immagine di copertina *</label>
             <input class="form-control @error('cover_photo') is-invalid @enderror" type="file" name="cover_photo"
-                id="cover_photo" accept="image/*">
+                id="cover_photo" value="{{ old('cover_photo')}}" accept="image/*">
             @error('cover_photo')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
+        </div>
 
-
-
-
-            {{-- visible --}}
-            <div class="mb-3">
-                <div>
-                    <label for="visible">Visible*</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input @error('flexRadioDefault1') is-invalid @enderror" type="radio"
-                        name="visible" id="flexRadioDefault1" value="1" required checked>
-                    <label class="form-check-label" for="flexRadioDefault1">
-                        Yes
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input @error('flexRadioDefault2') is-invalid @enderror" type="radio"
-                        name="visible" id="flexRadioDefault2" value="0" required>
-                    <label class="form-check-label" for="flexRadioDefault2">
-                        No
-                    </label>
-                </div>
-                @error('visible')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-
-
-
-
-
+        {{-- visible --}}
+        <div class="mb-3">
+            <div>
+                <label for="visible">Visibile *</label>
             </div>
+            <div class="form-check">
+                <input class="form-check-input @error('flexRadioDefault1') is-invalid @enderror" type="radio" name="visible"
+                id="flexRadioDefault1" value="1" required checked>
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Sì
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input @error('flexRadioDefault2') is-invalid @enderror" type="radio" name="visible"
+                    id="flexRadioDefault2" value="0" required>
+                <label class="form-check-label" for="flexRadioDefault2">
+                    No
+                </label>
+            </div>
+            @error('visible')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
 
-            {{-- checkbox --}}
-            <fieldset class="mb-3">
-                <legend>Services</legend>
+        {{-- checkbox Servizi --}}
+        <fieldset class="mb-3" id="myservices">
+            {{-- <legend>Services</legend> --}}
+            <label>Servizi *</label>
+            <br>
+            <div class=" mb-3 container row">
                 @foreach ($services as $service)
-                    <div class="form-check">
+                    <div class="form-check col-4">
                         {{-- <input type="hidden" name="services[]" value="0" /> --}}
-                        <input required class="form-check-input" type="checkbox" name="services[]"
-                            value="{{ $service->id }}" id="service{{ $service->id }}">
-                            {{-- @if (in_array($service->id, old('services') ?: [])) checked @endif --}}
+                        <input
+                            class="form-check-input"
+                            type="checkbox"
+                            name="services[]"
+                            value="{{ $service->id }}"
+                            id="service{{ $service->id }}">
+                            @if (in_array($service->id, old('services') ?: [])) checked @endif
                             {{-- @if ($service->name_services == 'Nessun servizio') checked @endif --}}
                         <label class="form-check-label"
                             for="service{{ $service->id }}">{{ $service->name_services }}</label>
                     </div>
                 @endforeach
-
+    
                 @foreach ($errors->get('services.*') as $messages)
                     @foreach ($messages as $message)
                         <div class="invalid-feedback d-block">
@@ -177,16 +168,15 @@
                     @endforeach
                 @endforeach
                 
-                @error('services')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
+            </div>
 
-            </fieldset>
+        </fieldset>
 
-            <button type="submit" class="btn btn-primary" onclick="validationHouses()">Save</button>
+        <button type="submit" class="btn btn-primary">Save
+        </button>
     </form>
+
     <script src="{{ asset('js/vue.js') }}" defer></script>
+
 @endsection
 
--- resources/views/admin/houses/edit.blade.php
+++ resources/views/admin/houses/edit.blade.php
@@ -2,16 +2,17 @@
 
 @section('mainContent')
     <h1>Modifica i dati della struttura</h1>
-    <form action="{{ route('admin.houses.update', ['house' => $house]) }}" method="post" novalidate enctype="multipart/form-data">
+    <form action="{{ route('admin.houses.update', ['house' => $house]) }}" method="post" novalidate enctype="multipart/form-data" id="form-edit-house">
         @method('put')
         @csrf
 
         {{-- nome casa --}}
         <div class="mb-3">
             <label class="form-label" for="name_house">Nome Casa *</label>
-            <input class="form-control @error('name_house') is-invalid @enderror" type="text" name="name_house" id="name_house" value="{{ old('name_house', $house->name_house)}}"  required  placeholder="inserisci il nome"
-            minlength="1"
-            maxlength="15">
+            <input class="form-control @error('name_house') is-invalid @enderror" type="text" name="name_house"
+                id="name_house" value="{{ old('name_house', $house->name_house)}}"  required  placeholder="inserisci il nome"
+                minlength="1"
+                maxlength="15">
             @error('name_house')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -22,93 +23,75 @@
         {{-- numero stanze  --}}
         <div class="mb-3">
             <label class="form-label" for="rooms">Numero Stanze *</label>
-            <input class="form-control @error('rooms') is-invalid @enderror" type="number" name="rooms" id="rooms" value="{{ old('rooms', $house->rooms)}}" required
-            min="1"
-            max="10" >
+            <input class="form-control @error('rooms') is-invalid
+            @enderror" type="number" name="rooms"
+                id="rooms" value="{{ old('rooms', $house->rooms)}}" required
+                min="1"
+                max="10" >
             @error('rooms')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
+
         {{-- numero letti --}}
         <div class="mb-3">
-            <label class="form-label" for="beds">Numero Letti *</label>
-            <input class="form-control @error('beds') is-invalid @enderror" type="number" name="beds" id="beds" value="{{ old('beds', $house->beds)}}" required
-            min="1"
-            max="10">
+            <label class="form-label" for="beds">Numero Letti (ospiti max) *</label>
+            <input class="form-control @error('beds') is-invalid
+            @enderror" type="number" name="beds"
+                id="beds" value="{{ old('beds', $house->beds)}}" required
+                min="1"
+                max="10">
             @error('beds')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
+
         {{-- numero bagni --}}
         <div class="mb-3">
             <label class="form-label" for="bathrooms">Numero Bagni *</label>
-            <input class="form-control @error('bathrooms') is-invalid @enderror" type="number" name="bathrooms" id="bathrooms" value="{{ old('bathrooms', $house->bathrooms)}}" required
-            min="1"
-            max="10">
+            <input class="form-control @error('bathrooms') is-invalid @enderror" type="number" name="bathrooms"
+                id="bathrooms" value="{{ old('bathrooms', $house->bathrooms)}}" required
+                min="1"
+                max="10">
             @error('bathrooms')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
+
         {{-- metri quadrati --}}
         <div class="mb-3">
             <label class="form-label" for="mq">Metri Quadrati *</label>
-            <input class="form-control @error('mq') is-invalid @enderror" type="number" name="mq" id="mq" value="{{ old('mq', $house->mq)}}" required
-            min="1"
-            max="500">
+            <input class="form-control @error('mq') is-invalid
+            @enderror" type="number" name="mq"
+                id="mq" value="{{ old('mq', $house->mq)}}" required
+                min="1"
+                max="500">
             @error('mq')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
-        {{-- <div class="mb-3">
-            <label class="form-label" for="lat">Latitudine</label>
-            <input class="form-control @error('lat') is-invalid @enderror" type="text" name="lat" id="lat" value="{{ old('lat', $house->lat) }}">
-            @error('title')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="lang">Longitudine</label>
-            <input class="form-control @error('lang') is-invalid @enderror" type="text" name="lang" id="lang" value="{{ old('lang', $house->lang) }}">
-            @error('title')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div> --}}
-        {{-- indirizzo --}}
-        {{-- <div class="mb-3">
-            <label class="form-label" for="address">Indirizzo *</label>
-            <input class="form-control @error('address') is-invalid @enderror" type="text" name="address" id="address" value="{{ old('address', $house->address)}}" required
-            minlength="1"
-            maxlength="20">
-             @error('address')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-             @enderror
-        </div> --}}
 
-
-        {{-- Indirizzo --}}
-        <div id="back">
+        {{-- Indirizzo con autocompletamento --}}
+        <div class="mb3" id="back">
             <find-address></find-address>
         </div>
+        <br>
 
         {{-- tipo di struttura --}}
         <div class="mb-3">
             <label class="form-label" for="type">Tipo di Struttura: *</label>
-            <input class="form-control @error('type') is-invalid @enderror" type="text" name="type" id="type" value="{{ old('type', $house->type)}}"
-            required pattern="[Aa]ppartamento|[Aa]ttico|[Vv]illa|[Mm]asseria|[Cc]asale" placeholder="Appartamento, Attico, Villa, Masseria, Casale.">
+            <input class="form-control @error('type') is-invalid
+            @enderror" type="text" name="type" 
+                id="type" value="{{ old('type', $house->type)}}" required
+                pattern="[Aa]ppartamento|[Aa]ttico|[Vv]illa|[Mm]asseria|[Cc]asale" placeholder="Appartamento, Attico, Villa, Masseria, Casale.">
             @error('type')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -118,30 +101,33 @@
 
         {{-- immagini --}}
         <div class="mb-3">
-            <label class="form-label" for="cover_photo">Image</label>
-            <input class="form-control @error('cover_photo') is-invalid @enderror" type="file" name="cover_photo" id="cover_photo" accept="image/*">
+            <label class="form-label" for="cover_photo">Immagine di copertina *</label>
+            <input class="form-control @error('cover_photo') is-invalid @enderror" type="file" name="cover_photo"
+                id="cover_photo" value="{{ old('cover_photo, $house->cover_photo')}}"accept="image/*">
             @error('cover_photo')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
-
-
         </div>
 
-                {{-- visible --}}
+        {{-- visible --}}
         <div class="mb-3">
             <div>
-                <label for="visible">Visible*</label>
+                <label for="visible">Visibile *</label>
             </div>
             <div class="form-check">
-                <input class="form-check-input @error('flexRadioDefault1') is-invalid @enderror" type="radio" name="visible" id="flexRadioDefault1" value="1" required checked>
+                <input class="form-check-input @error('flexRadioDefault1') is-invalid
+                @enderror" type="radio" name="visible"
+                id="flexRadioDefault1" value="1" required checked>
                 <label class="form-check-label" for="flexRadioDefault1">
-                  Yes
+                  Sì
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input @error('flexRadioDefault2') is-invalid @enderror" type="radio" name="visible" id="flexRadioDefault2" value="0" required>
+                <input class="form-check-input @error('flexRadioDefault2') is-invalid
+                @enderror" type="radio" name="visible"
+                    id="flexRadioDefault2" value="0" required>
                 <label class="form-check-label" for="flexRadioDefault2">
                   No
                 </label>
@@ -152,36 +138,44 @@
                     {{ $message }}
                 </div>
             @enderror
+        </div>
 
-       {{-- checkbox --}}
-       <fieldset class="mb-3">
-        <legend>Services</legend>
-        @foreach ($services as $service)
-            <div class="form-check">
-                {{-- <input type="hidden" name="services[]" value="0" /> --}}
-                <input
-                    required
-                    class="form-check-input"
-                    type="checkbox"
-                    name="services[]"
-                    value="{{ $service->id }}"
-                    id="service{{ $service->id }}"
-                    @if(in_array($service->id, old('services', $house->services->pluck('id')->all()) ?: [])) checked @endif
-                >
-                <label class="form-check-label" for="service{{ $service->id }}">{{ $service->name_services }}</label>
-            </div>
-        @endforeach
-
-        @foreach ($errors->get('services.*') as $messages)
-            @foreach ($messages as $message)
-                <div class="invalid-feedback d-block">
-                    {{ $message }}
+        {{-- checkbox Servizi--}}
+        <fieldset class="mb-3" id="myservices">
+        {{-- <legend>Services</legend> --}}
+        <label>Servizi *</label>
+        <br>
+        <div class=" mb-3 container row">
+            @foreach ($services as $service)
+                <div class="form-check col-4">
+                    {{-- <input type="hidden" name="services[]" value="0" /> --}}
+                    <input
+                        class="form-check-input"
+                        type="checkbox"
+                        name="services[]"
+                        value="{{ $service->id }}"
+                        id="service-{{ $service->id }}">
+                        @if(in_array($service->id, old('services', $house->services->pluck('id')->all()) ?: [])) checked @endif
+                    <label class="form-check-label"
+                        for="service{{ $service->id }}">{{ $service->name_services }}</label>
                 </div>
             @endforeach
-        @endforeach
+
+            @foreach ($errors->get('services.*') as $messages)
+                @foreach ($messages as $message)
+                    <div class="invalid-feedback d-block">
+                        {{ $message }}
+                    </div>
+                @endforeach
+            @endforeach
+
+        </div>
+
     </fieldset>
 
         <button type="submit" class="btn btn-primary" onclick="validationHouses()">Modifica dati</button>
     </form>
+
     <script src="{{ asset('js/vue.js') }}" defer></script>
+
 @endsection
