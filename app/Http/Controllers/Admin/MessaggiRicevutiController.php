7ca5c9013e717ee0ea339d781713a9435059edcf
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-09-26 10:25:00 +0200

    agg

---
+++ app/Http/Controllers/Admin/MessaggiRicevutiController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessaggiRicevutiController extends Controller
+{
+    public function index()
+    {
+        $messages= Message::All()->where('house_id', auth()->user()->id);;
+
+        return view('admin.messages.index', compact('messages'));
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Message $message)
+    {
+        //
+    }
+
+
+    public function edit(Message $message)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+
+    public function destroy(Message $message)
+    {
+        //
+    }
+}
