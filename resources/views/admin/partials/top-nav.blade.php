3edccce7b041c377b0b488bf9ec4f669f25a5411
LeppoXena <francesco.lepore@xenagames.com>
2022-09-27 00:13:22 +0200

    Navbar ritornata scura, aggiunta cagate al footer

--- resources/views/admin/partials/top-nav.blade.php
+++ resources/views/admin/partials/top-nav.blade.php
@@ -1,20 +1,20 @@
-<nav class="navbar navbar-expand-lg navbar-expand-sm navbar-light">
+<nav class="navbar navbar-expand navbar-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('home') }}">
-            <img src="{{ asset ('storage/logo/BBB ext large Alt.png')}}" alt="" height="24">
+            <img src="{{ asset ('storage/logo/BBB ext large Alt.png')}}" alt="BBB logo" height="24">
         </a>
-        {{-- <img src="https://cdn.discordapp.com/attachments/1013789688995258529/1018927773735207003/Tavola_disegno_13x.png" alt="" width="100%" height="24"> --}}
         <div class="collapse navbar-collapse justify-content-end">
-            <li class="list-unstyled me-4">
-                <a class="nav-link btn btn-light p-1" href="{{ route('logout') }}"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                >
-                    Logout
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -10,75 +10,93 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-expand-sm navbar-light" style="background-color: #993923;">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand ms-3" href="{{ route('home') }}">
-                {{-- <img src="https://cdn.discordapp.com/attachments/1013789688995258529/1018927773735207003/Tavola_disegno_13x.png" alt="" width="100%" height="24"> --}}
-                <img src="{{ asset('storage/logo/BBB logo large NEG.png') }}" alt="" height="50">
-
+            <a class="navbar-brand" href="{{ route('home') }}">
+                <img src="{{ asset('storage/logo/BBB ext large Alt.png') }}" alt="BBB logo" height="24">
             </a>
-
             @if (Route::has('login'))
-                <div class="collapse navbar-collapse justify-content-end">
-                    @auth
-                        <a class="nav-link active btn btn-light mr-3" href="{{ route('admin.houses.index') }}">Vai ai tuoi
-                            Appartamenti</a>
-                        <a class="nav-link btn btn-light" href="{{ route('logout') }}"
-                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            Logout
+            <div class="collapse navbar-collapse justify-content-end">
+                @auth
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active mr-3" href="{{ route('admin.houses.index') }}">
+                            Vai ai tuoi
+                            Appartamenti
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
-                    @else
-                        <li class="list-unstyled me-4">
-                            <a class="nav-link active btn btn-light mr-3" href="{{ route('login') }}">Login</a>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a class="nav-link active text-white" href="#">Ciao</a>
+                    </li> --}}
+                </ul>
+                @else
+                <ul class="navbar-nav mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active mr-3" href="{{ route('login') }}">Accedi</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ route('register') }}">Registrati</a>
                         </li>
-
-                        @if (Route::has('register'))
-                            <li class="list-unstyled me-2">
-                                <a class="nav-link active btn btn-light" href="{{ route('register') }}">Register</a>
-                            </li>
-                        @endif
-                    @endauth
-                </div>
+                    @endif
+                </ul>
+                @endauth
+            </div>
             @endif
         </div>
     </nav>
-
 
     <div id="root"></div>
 
     <footer style="background-color: black" class="text-white">
         <div class="container-fluid text-center pt-5">
             <div class="row d-flex justify-content-between">
-                <div class="col-sx-12 col-sm-4 col-lg-4 mb-4">
-                    <h6>LOCATION</h6>
-                    <p>Indirizzo</p>
+                <div class="col-sx-12 col-sm-4 col-lg-4 mb-4 small">
+                    <h6>BBB srl</h6>
+                    <span>Sede legale :</span>
+                    <em>Via Gennaro capuozzo 21/12</em>
+                    <br>
+                    <span>P.Iva :</span>
+                    <em>12345678</em>
+
                 </div>
                 <div class="d-flex flex-column align-items-center col-sx-12 col-sm-4 col-lg-4 mb-5 px-2">
-                    <h4>WEB</h4>
+                    <h6>Cercaci</h6>
                     <div class="d-flex">
-                        <div class="icons border border-white rounded-circle px-1 py-1 m-1">
-                            <i class="fab fa-facebook-f"></i>
+                        <div class="icons mx-4">
+                            <img src="{{ asset ('storage/logo/FB2.png')}}" alt="" height="22">
                         </div>
-                        <div class="icons border border-white rounded-circle px-1 py-1 m-1">
-                            <i class="fab fa-google-plus-g"></i>
+                        <div class="icons mx-4">
+                            <img src="{{ asset ('storage/logo/Ld.png')}}" alt="" height="22">
                         </div>
-                        <div class="icons border border-white rounded-circle px-1 py-1 m-1">
-                            <i class="fab fa-twitter"></i>
-                        </div>
-                        <div class="icons border border-white rounded-circle px-1 py-1 m-1">
-                            <i class="fab fa-linkedin-in"></i>
+                        <div class="icons mx-4">
+                            <img src="{{ asset ('storage/logo/Insta.png')}}" alt="" height="22">
                         </div>
                     </div>
                 </div>
-                <div class="col-sx-12 col-sm-4 col-lg-4 mb-5">
-                    <h6>ABOUT</h6>
-                    <p>Text</p>
+                <div class="col-sx-12 col-sm-4 col-lg-4 mb-5 small">
+                    <h6>Bool BnB</h6>
+                    <span>Telefono :</span>
+                    <em>010 314164</em>
+                    <br>
+                    <span>E-Mail :</span>
+                    <em>support@bbb.com</em>
                 </div>
             </div>
         </div>
